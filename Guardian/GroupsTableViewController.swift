f5bc1ebfeff61b30027111a7f67e40463e171697
henryboswell <Franklin.henry.boswell@gmail.com>
2017-01-04 23:48:26 -0500

    added http requests and keychain

--- Guardian/GroupsTableViewController.swift
+++ Guardian/GroupsTableViewController.swift
@@ -22,6 +22,8 @@ class GroupsTableViewController: UITableViewController {
         super.viewDidLoad()
         initalizeFetchedResultsController()
         tableView.reloadData()
+        getGroupDataFromServer()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -36,6 +38,73 @@ class GroupsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    func getGroupDataFromServer(){
+        
+        let token = KeychainController.loadToken()!
+        print(token)
+        
+        
+        //let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.nnhd7iTOuy-h-RH8pP_37KcQmkgDq4m3dsIgkY4IwT0"
+        
+      
+
+        
+        var request = URLRequest(url: URL(string: "https://guardian-app-v1.herokuapp.com/api/v1/groupsapi/1")!)
+
+        
+        request.httpMethod = "GET"
+        request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            //let responseString = String(data: data, encoding: .utf8)
+            DispatchQueue.main.async {
+            
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:AnyObject]]
+                    print(json?[1])
+                    
+                    for group in json! {
+                        let addGroup = group as! [String:AnyObject]
+                        
+                        
+                        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                        
+                        let group = NSEntityDescription.insertNewObject(forEntityName: "Group", into: moc) as! Group
+                        group.title = addGroup["title"] as! String?
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                    
+                    }
+                }catch{
+              
+                }
+            }
+            
+        }
+        task.resume()
+        
+        
+        
+    }
+    
+    
+    
     
     func initalizeFetchedResultsController(){
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Group")
--- Guardian/ViewController.swift
+++ Guardian/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,80 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var EmailField: UITextField!
+    
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        
+        
+        var request = URLRequest(url: URL(string: "https://guardian-app-v1.herokuapp.com/api/v1/auth_user")!)
+        request.httpMethod = "POST"
+        
+        
+        
+        var postString = "email="
+        postString += EmailField.text!
+        postString += "&password="
+        postString += PasswordField.text!
+        
+        
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            //let responseString = String(data: data, encoding: .utf8)
+            do{
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+                print(json["auth_token"]!)
+                self.loginSucessful(token: json["auth_token"]! as! NSString)
+            }catch{
+               self.loginUnsucessful()
+            }
+            
+            //print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
+    
+    func loginUnsucessful(){
+        DispatchQueue.main.async {
+            self.alert(message: "Wrong Username or Password")
+        }
+    }
+    
+    func loginSucessful(token: NSString){
+        
+        DispatchQueue.main.async {
+            KeychainController.saveToken(token: token)
+            let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: moc) as! User
+            user.token = token as String
+            self.performSegue(withIdentifier: "login", sender: self)
+        }
+        
+        
+    }
+    
 
-
+}
+extension UIViewController {
+    
+    func alert(message: String, title: String = "") {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
