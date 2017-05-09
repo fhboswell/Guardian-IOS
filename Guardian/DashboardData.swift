21d9252bf00c248061577c80d78503922b9be4ed
fhboswell <franklin.henry.boswell@gmail.com>
2017-05-08 21:41:32 -0700

    Added view, view controller and data for Dashboard

---
+++ Guardian/DashboardData.swift
@@ -0,0 +1,244 @@
+//
+//  DashboardData.swift
+//  Guardian
+//
+//  Created by Henry Boswell on 5/8/17.
+//  Copyright © 2017 com.project. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Foundation
+
+
+class DashboardData  {
+    
+    
+    static let sharedInstance = DashboardData()
+    func purge(_ entityName:String){
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let venueFetch = NSFetchRequest<NSFetchRequestResult>(entityName: entityName )
+        do {
+            let fetched = try moc.fetch(venueFetch) as! [NSManagedObject]
+            fetched.forEach({moc.delete($0)})
+            
+        } catch {
+            print("Something went wrong.\n")
+        }
+        
+        
+    }
+    
+    
+    var group :String?
+    
+    func getIndividualDataFromServer(group :String){
+        purge("Individual")
+        
+        
+        self.group = group
+        let token = KeychainController.loadToken()!
+        print(token)
+        
+        var urlString = URLModel.sharedInstance.baseUrl
+        
+        urlString += group
+        urlString += "/individualsapi"
+        
+        print(urlString)
+        var request = URLRequest(url: URL(string: urlString)!)
+        
+        
+        request.httpMethod = "GET"
+        request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            //let responseString = String(data: data, encoding: .utf8)
+            DispatchQueue.main.async {
+                
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:AnyObject]]
+                    //print(json?[0] as Any)
+                    
+                    for individual in json! {
+                        let addIndividual = individual
+                        
+                        
+                        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                        
+                        let individual = NSEntityDescription.insertNewObject(forEntityName: "Individual", into: moc) as! Individual
+                        individual.name = addIndividual["name"] as! String?
+                        individual.check = addIndividual["check"] as! String?
+                        individual.id = String(addIndividual["id"] as! Int)
+                        //print(individual.check ?? "default value")
+                        
+                        
+                    }
+                }catch{
+                    DispatchQueue.main.async {
+                        //self.alert(message: "website error")
+                    }
+                    
+                }
+            }
+            //self.fetchAndEdit()
+        }
+        task.resume()
+        
+        
+        
+        
+    }
+    
+    
+    
+    func fetchAndEdit(group :Int, name :String, check :String){
+        //if(group == self.group){
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Individual")
+        //let name = "Henry"
+        fetchRequest.predicate = NSPredicate(format: "name == %@", name)
+        
+        do {
+            let fetchedEmployees = try moc.fetch(fetchRequest) as! [Individual]
+            fetchedEmployees.first?.check = check
+            print(fetchedEmployees)
+        } catch {
+            fatalError("Failed to fetch employees: \(error)")
+        }
+        // }
+        
+    }
+    
+    
+    
+    
+    
+    func changeCheckInStatus(group :String, individual :String){
+        self.group = group
+        let token = KeychainController.loadToken()!
+        print(token)
+        
+        var urlString = URLModel.sharedInstance.baseUrl
+        
+        urlString += group
+        urlString += "/individualsapi/"
+        urlString += individual
+        urlString += "/change"
+        
+        
+        print(urlString)
+        var request = URLRequest(url: URL(string: urlString)!)
+        
+        
+        request.httpMethod = "PATCH"
+        request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let _ = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+        }
+        task.resume()
+        
+        
+        
+        
+    }
+    
+    
+    
+    func createIndividualWithGuardian(email :String, name :String){
+        
+        //self.group = group
+        let token = KeychainController.loadToken()!
+        print(token)
+        
+        var urlString = URLModel.sharedInstance.baseUrl
+        
+        urlString += group!
+        urlString += "/individualsapi"
+        
+        print(urlString)
+        var request = URLRequest(url: URL(string: urlString)!)
+        
+        
+        request.httpMethod = "POST"
+        request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        var postString = "individual[email]="
+        postString += email
+        postString += "&individual[name]="
+        postString += name
+        
+        
+        
+        request.httpBody = postString.data(using: .utf8)
+        print(request.httpBody)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 204 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }else{
+                
+            }
+            
+            //let responseString = String(data: data, encoding: .utf8)
+            DispatchQueue.main.async {
+                
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:AnyObject]]
+                    print(json?[0] as Any)
+                    
+                    for individual in json! {
+                        let addIndividual = individual
+                        
+                        
+                        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                        
+                        let individual = NSEntityDescription.insertNewObject(forEntityName: "Individual", into: moc) as! Individual
+                        individual.name = addIndividual["name"] as! String?
+                        individual.check = addIndividual["check"] as! String?
+                        individual.id = String(addIndividual["id"] as! Int)
+                        //print(individual.check ?? "default value")
+                        
+                        
+                    }
+                }catch{
+                    DispatchQueue.main.async {
+                        //self.alert(message: "website error")
+                    }
+                    
+                }
+            }
+            //self.fetchAndEdit()
+        }
+        task.resume()
+    }
+    
+    
+    
+}
---
+++ Guardian/DashboardViewController.swift
@@ -0,0 +1,153 @@
+//
+//  DashboardViewController.swift
+//  Guardian
+//
+//  Created by Henry Boswell on 5/8/17.
+//  Copyright © 2017 com.project. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DashboardViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate{
+    
+    
+    
+    @IBOutlet weak var DashboardTableView: UITableView!
+    
+    
+    var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        DashboardTableView.delegate = self
+        DashboardTableView.dataSource = self
+        initalizeFetchedResultsController()
+        
+        
+        // IndividualData.sharedInstance.getIndividualDataFromServer(group: group)
+        
+        
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+       override func viewDidAppear(_ animated: Bool) {
+        //DashboardData.sharedInstance.getDashboardDataFromServer()
+    }
+    
+    
+    
+    
+    func initalizeFetchedResultsController(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Individual")
+        let sort = NSSortDescriptor(key: "name", ascending: false)
+        request.sortDescriptors = [sort]
+        
+        let sortAgain = NSSortDescriptor(key: "check", ascending: false)
+        request.sortDescriptors = [sortAgain]
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self as! NSFetchedResultsControllerDelegate
+        do{
+            try fetchedResultsController.performFetch()
+            print(fetchedResultsController.fetchedObjects ?? "default value")
+        }catch{
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
+        
+    }
+    
+    func configureCell(cell: UITableViewCell, indexPath: IndexPath) {
+        guard let selectedIndividual = fetchedResultsController.object(at: indexPath) as? Individual
+            else{
+                fatalError("Failed to initialize ")
+        }
+        cell.textLabel?.text = selectedIndividual.name
+        if(selectedIndividual.check == "No"){
+            cell.backgroundColor = UIColor.red
+            print("turn it red")
+        }else {
+            cell.backgroundColor = UIColor.green
+            print("turn it green")
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections!.count
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = fetchedResultsController.sections else {
+            fatalError("No sections in fetchedResultsController")
+        }
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cellidentifier2", for: indexPath)
+        // Set up the cell
+        configureCell(cell: cell, indexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        
+        guard let selectedIndividual = fetchedResultsController.object(at: indexPath) as? Individual
+            else{
+                fatalError("Failed to initialize ")
+        }
+        // cell.textLabel?.text = selectedIndividual.name
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+    }
+    
+    
+}
+
+extension DashboardViewController:NSFetchedResultsControllerDelegate{
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        DashboardTableView.beginUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            DashboardTableView.insertSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+        case .delete:
+            DashboardTableView.deleteSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+        case .move:
+            break
+        case .update:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            DashboardTableView.insertRows(at: [newIndexPath! as IndexPath], with: .fade)
+        case .delete:
+            DashboardTableView.deleteRows(at: [indexPath! as IndexPath], with: .fade)
+        case .update:
+            configureCell(cell: DashboardTableView.cellForRow(at: indexPath! as IndexPath)!, indexPath: indexPath! as IndexPath)
+        case .move:
+            //configureCell(cell: tableView.cellForRow(at: indexPath! as IndexPath)!, indexPath: indexPath! as IndexPath)
+            //tableView.moveRow(at: indexPath!, to: newIndexPath! as IndexPath)
+            DashboardTableView.reloadData()
+            
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        IndividualTableView.endUpdates()
+    }
+}
